e86b69ccbcd845e5b23ef80860e22521439ed2e4
Adam Brodin <worecraft@gmail.com>
2020-10-06 17:16:09 +0200

    Code Style

---
+++ Code Style/Exercise 1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CodeStyle
+{
+    public class Person // Changed persons to Person (capital letter for classes & fixed grammar mistake)
+    {
+        public string firstName; // first_name to firstName
+        public string lastName;
+        public int age; // AGE to age (shouldn't be caps)
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.Write("First name: ");
+            string firstName = Console.ReadLine(); // unneccessary to name str (strFirstName -> firstName)
+
+            Console.Write("Last name: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Age: ");
+            int age = int.Parse(Console.ReadLine()); // confusing variable name (x -> age)
+
+            Person p = new Person
+            {
+                firstName = firstName,
+                lastName = lastName,
+                age = age
+            };
+
+            if (p.age == 20)
+            {
+                Console.WriteLine("You are 20");
+            }
+            else // else if to else, unneccessarily long expression
+            {
+                Console.WriteLine("You are not 20");
+            }
+        }
+    }
+}
